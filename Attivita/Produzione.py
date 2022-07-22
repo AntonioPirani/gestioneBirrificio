304ac21ee748140ac37913816c5caee61db00518
Matteo Seresi <s1100028@studenti.univpm.it>
2022-07-22 15:56:40 +0200

    Aggiornamenti di alcuni metodi.

--- Attivita/Produzione.py
+++ Attivita/Produzione.py
@@ -1,6 +1,7 @@
 import datetime
-import os.path
+from datetime import timedelta
 import pickle
+import os
 import pandas as pd
 
 class Produzione:
@@ -17,20 +18,20 @@ class Produzione:
         self.composto =""
 
 
-    def inizioLavorazione(self, codiceProduzione, note):
+    def inizioLavorazione(self, codiceProduzione):
         self.codiceProduzione = codiceProduzione
-        self.note = note
+        self.note = ""
         self.dataInizio = datetime.date.today()
         self.dataFine = self.dataInizio + pd.DateOffset(days=1) #la fine della produzione avviene dopo un mese dall'inizio (necessita di scaricarsi il pacchetto dateutil)
         self.livello=1;
         self.temperatura=18.0
         self.composto = "Composto"+str(codiceProduzione)
         produzioni = {}
-        if os.path.isfile('Dati/Produzione.pickle'):
-            with open('Dati/Produzione.pickle', 'rb') as f:
+        if os.path.isfile('Dati/Produzioni.pickle'):
+            with open('Dati/Produzioni.pickle', 'rb') as f:
                 produzioni = pickle.load(f)
         produzioni[codiceProduzione] = self
-        with open('Dati/Produzione.pickle', 'wb') as f:
+        with open('Dati/Produzioni.pickle', 'wb') as f:
             pickle.dump(produzioni, f, pickle.HIGHEST_PROTOCOL)
 
     def visualizzaProduzione(self):
@@ -44,47 +45,48 @@ class Produzione:
             "comnposto": self.composto,
         }
 
-    def rimuoviProduzione(self):
-        if os.path.isfile('Dati\Produzione.pickle'):
-            with open('Dati\PProduzione.pickle', 'wb+') as f:
-                produzione = pickle.load(f)
-                del produzione[self.codiceProduzione]
-                pickle.dump(produzione, f, pickle.HIGHEST_PROTOCOL)
-        self.codiceProduzione = -1
-        self.dataInizio = datetime.datetime(1970, 1, 1, 0, 0)
-        self.dataFine = datetime.datetime(1970, 1, 1, 0, 0)
-        self.livello = -1
-        self.materieUtilizzate = None
-        self.note = ""
-        self.prodotto = None
-        self.temperatura = -1.0
-        self.composto = ""
-        del self
-
-
     def ricercaProduzione(self, codiceProduzione):
-        if os.path.isfile('Dati/Produzione.pickle'):
-            with open('Dati/Produzione.pickle', 'rb') as f:
+        if os.path.isfile('Dati/Produzioni.pickle'):
+            with open('Dati/Produzioni.pickle', 'rb') as f:
                 produzioni = pickle.load(f)
                 return produzioni[codiceProduzione]
         else:
             return None
 
-    def controllaProdotto(self, codiceProduzione, temperatura, livello, composto, dataInizio, dataFine): #meglio chiamare il metodo in controllaProduzione
-        while dataInizio < dataFine:
-            print(temperatura)
-            print(livello)
-            print(composto)
-            dataInizio = dataInizio + pd.DateOffset(hours=4)
-            if temperatura > 100 or livello < 2 or composto != "Composto"+str(codiceProduzione): #i numeri sono a caso
-                self.segnalaAnomalia(codiceProduzione)
+    def rimuoviProduzione(self):
+        if os.path.isfile('Dati/Produzioni.pickle'):
+            with open('Dati/Produzioni.pickle', 'rb') as f:
+                produzioni = pickle.load(f)
+                del produzioni[self.codiceProduzione]
+                with open('Dati/Produzioni.pickle', 'wb') as handle:
+                    pickle.dump(produzioni, handle, pickle.HIGHEST_PROTOCOL)
+                self.codiceProduzione = -1
+                self.dataInizio = datetime.datetime(1970, 1, 1, 0, 0)
+                self.dataFine = datetime.datetime(1970, 1, 1, 0, 0)
+                self.livello = -1
+                self.materieUtilizzate = None
+                self.note = ""
+                self.prodotto = None
+                self.temperatura = -1.0
+                self.composto = ""
+                del self
 
-    def segnalaAnomalia(self, codiceProduzione):
-        print("SEGNALATA ANOMALIA")
+    def controllaProdotto(self, codiceProduzione, temperatura, livello, composto, dataInizio, dataFine): #meglio chiamare il metodo in controllaProduzione
+        while dataInizio <= dataFine:
+            #print("Controllo alle ore: "+dataInizio.strftime("%H")+" alla data "+dataInizio.strftime("%Y-%m-%d"))
+            dataInizio = dataInizio + pd.DateOffset(hours=4)
+            if temperatura > 100 or livello < 2 or composto!="Composto"+str(codiceProduzione): #i numeri sono a caso
+                #self.segnalaAnomalia(codiceProduzione)
+                print("SEGNALATA ANOMALIA, ")
+                temperatura = 50
+                livello = 3
+                composto = "Composto" + str(codiceProduzione)
+
+    def segnalaAnomalia(self, codiceProduzione): #non funziona
+        print("SEGNALATA ANOMALIA, ")
         self.temperatura = 50
-        self.livello = 1
+        self.livello = 3
         self.composto="Composto"+str(codiceProduzione)
-        return True
 
     def registraProdotto(self, codiceProduzione, dataInizio, dataFine, livello, materieUtilizzate, note, prodotto, temperatura, composto):
         self.codiceProduzione = codiceProduzione
